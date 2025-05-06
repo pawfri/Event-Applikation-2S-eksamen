9630114d2149ea9904b5f086e8fe24688beebb27
mikkaakdag <mikkaakdag@gmail.com>
2025-05-06 12:39:12 +0200

    TIlføjet nyt .cshtml for Nicki Minaj siden

---
+++ Pages/Shared/NickiMinaj.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Event_Applikation.Pages.Shared
+{
+    public class NickiMinajModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
