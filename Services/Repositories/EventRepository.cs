85c17f81138c1c161595c26a2f7a6a79a3e677b6
Valde2609 <valde2609@gmail.com>
2025-05-08 15:08:04 +0200

    fortsat forsøg på event side TODO - kunne oprette events som gemmes i DB

--- Services/Repositories/EventRepository.cs
+++ Services/Repositories/EventRepository.cs
@@ -6,10 +6,28 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Event_Applikation.Services.Repositories;
 
-public class EventRepository : Repository<Event>, IEventRepository
+//public class EventRepository : Repository<Event>, IEventRepository
+//{
+//    protected override DbContext CreateDbContext()
+//    {
+//        return new mvp2_dk_db_eventapplikationContext();
+//    }
+//}
+
+
+
+public class EventRepository : IEventRepository
 {
-    protected override DbContext CreateDbContext()
+    private readonly mvp2_dk_db_eventapplikationContext _context;
+
+    public EventRepository(mvp2_dk_db_eventapplikationContext context)
     {
-        return new mvp2_dk_db_eventapplikationContext();
+        _context = context;
+    }
+
+    public void Create(Event @event)
+    {
+        _context.Events.Add(@event);
+        _context.SaveChanges(); // This writes to the database
     }
 }
