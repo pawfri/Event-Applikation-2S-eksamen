e0004e213eca0c57f87049c6161fa2884769202f
Valde2609 <valde2609@gmail.com>
2025-05-15 12:47:38 +0200

    Implementeret Admins evne til at oprette nye brugere af typerne: Brugere, Medlemmer & Administrator

---
+++ Pages/Brugere/Admin/Create.cshtml.cs
@@ -0,0 +1,64 @@
+using Event_Applikation.Models;
+using Event_Applikation.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace Event_Applikation.Pages.Brugere.Admin
+{
+    public class CreateModel : PageModel
+    {
+		private IBrugerRepository _brugerRepo;
+		private readonly mvp2_dk_db_eventapplikationContext _context;
+
+		[BindProperty]
+		public Bruger NewBruger { get; set; }
+
+        public SelectList RolleList { get; set; }
+
+        public SelectList CampusList { get; set; }
+
+        public CreateModel(mvp2_dk_db_eventapplikationContext context, IBrugerRepository brugerrepo)
+		{
+			_context = context;
+			_brugerRepo = brugerrepo;
+		}
+
+        /// <summary>
+        /// Når Create-siden hentes kaldes metoden: "LoadRollerCampus", 
+        /// så de kan benyttes på sidens dropdown.
+        /// </summary>
+        public void OnGet()
+        {
+			LoadRollerCampus();
+        }
+
+        /// <summary>
+        /// Håndterer at kontrollere og indsende input, for at oprette en ny bruger.
+        /// </summary>
+        public IActionResult OnPostSubmit()
+		{
+			//Kontrol af gyldig data
+			if (!ModelState.IsValid || NewBruger == null)
+			{
+				OnGet();
+				return Page();
+			}
+
+			//Opretter en ny bruger og omdirigerer til Forsiden
+			_brugerRepo.Create(NewBruger);
+			return RedirectToPage("/Index");
+        }
+
+        /// <summary>
+        /// Indlæser roller og Campus fra databasen, for at de kan anvendes
+        /// i drop-down menu i applikationen. Kaldes i OnGet.
+        /// </summary>
+        public void LoadRollerCampus()
+        {
+            RolleList = new SelectList(_context.Rolles, "Id", "Brugertype");
+            CampusList = new SelectList(_context.Campuses, "Id", "By");
+        }
+    }
+}
